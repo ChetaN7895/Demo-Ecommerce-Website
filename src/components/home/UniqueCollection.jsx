c43a7476084c8b331aa0de2c6553cf9197575226
Chetan Solanki <151900157+ChetaN7895@users.noreply.github.com>
2025-05-29 17:19:39 +0530

    Update UniqueCollection.jsx

--- src/components/home/UniqueCollection.jsx
+++ src/components/home/UniqueCollection.jsx
@@ -27,7 +27,8 @@ const UniqueCollection = () => {
         <div className="max-w-7xl mx-auto">
           <div className="text-center max-w-xl mx-auto">
             <h2 className="text-3xl font-extrabold  inline-block font-serif">Unique Collection</h2>
-            <p className="text-sm mt-6 dark:text-gray-200 font-serif">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan, nunc et tempus blandit, metus mi consectetur felis turpis vitae ligula.</p>
+            <p className="text-sm mt-6 dark:text-gray-200 font-serif">Not your average fashion drop.
+Unique fits, fierce looks — made to turn heads..</p>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12 max-md:max-w-lg mx-auto">
           {products.map((product) => (
